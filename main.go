983221fef2072c32a1f14deb052fe51b0c861c8e
Ricardo <jony.ricardo95@gmail.com>
2019-03-17 15:48:21 +0000

    Comments
    Comments added to code

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 
 func main() {
 
-	RamUsage()
+	//RamUsage()
 
     var value int
     value = getValues()
     fmt.Println(value)
 }
-
+/*
 //Function that gathers the Ram Usage
 func RamUsage() {
         var m runtime.MemStats
@@ -30,9 +30,11 @@ func RamUsage() {
 func bToMb(b uint64) uint64 {
     return b / 1024 / 1024
 }
+*/
 
+//Function that generates random integer values
 func getValues() int {
 
-    rand.Seed(time.Now().UnixNano())
-    return rand.Int()
+    rand.Seed(time.Now().UnixNano())   //Using current time in nanosenconds as a seed so it changes everytime
+    return rand.Int()                  //Generating random value
 }
