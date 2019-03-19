a8e00e40197f407cc3adb49dcc8cf9291cba948b
Ricardo <jony.ricardo95@gmail.com>
2019-03-19 15:48:39 +0000

    Write and reading working v1
    Write and deletes file constantly, ram and disk intensive, read from top

--- main.go
+++ main.go
@@ -8,10 +8,15 @@ import (
     "os"
     "log"
     "bufio"
+    "strings"
+    "io/ioutil"
+    "strconv"
 )
 
-const samp_len = 4      // "Sensor" Sample size
-const fileName = "dat1.txt"
+const samp_len = 4                          // "Sensor" Sample size
+const fileName = "dat1.txt"                 // Name of the file with the samples
+var flagGo bool = false                     // Free to go boolean
+var reader = bufio.NewReader(os.Stdin)      // Standard input (keyboard) reader
 
 func main() {
 
@@ -19,48 +24,8 @@ func main() {
     initializeFile()
     
     go generator()
-    reader := bufio.NewReader(os.Stdin)
-    cmdString, err := reader.ReadString('\n')
-    check(err)
 
-    fmt.Println(cmdString)    
-}
-
-//Function that runs in "background" generating samples each second
-func generator() {
-
-    i := 10
-    for i>1 {                           //Infinite loop
-        var sample [samp_len] int       //Array to save vaules
-
-        sample = getValues()            //Getting sample values
-        writeFile(sample)               //Writing samples to file
-        time.Sleep(time.Second)         //Waiting one second
-    }
-}
-
-//Function that writes to the File that will be used to save values
-func writeFile(w [samp_len]int) {
-
-    f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755) // Opens file with permission and appends new values
-    check(err)
-    defer f.Close()
-
-    for _, value := range w {
-        _, err = f.WriteString(fmt.Sprintf("%d\n", value))
-       check(err)
-
-    }
-
-}
-
-//Function that initializes file
-func initializeFile() {
-    f, err := os.Create(fileName)
-    check(err)
-
-    defer f.Close()
-   
+    userInterface ()
 }
 /*
 //Function that gathers the Ram Usage
@@ -78,6 +43,121 @@ func bToMb(b uint64) uint64 {
     return b / 1024 / 1024
 }
 */
+
+//Function that manages the user interface
+func userInterface() {
+
+    fmt.Println("\nWELCOME TO THE SENSOR SIMULATOR\n")
+    var input string
+    var argv []string
+    for flagGo == false {}
+    i := 10
+    for i>1 {
+        printMenu()
+        input = readInput()
+        if len(input) < 3 {
+            argv[0] = "error"
+        } else{
+            argv = strings.Fields(input)
+        }
+        switch argv[0] {
+        case "all" :
+            allFunc(argv)
+        /*case "some" :
+            someFunc(argv)
+        case "average" :
+            averFunc(argv)
+        */default:
+            fmt.Println("Unexpected input")
+        }
+    }
+    fmt.Println(argv)
+}
+
+//Function that handles the case to show all variables
+func allFunc(argv [] string) {
+
+    fmt.Println("All function")
+    fileHandle, _ := os.Open(fileName)
+    defer fileHandle.Close()
+    fileScanner := bufio.NewScanner(fileHandle)
+
+    tmp, err := strconv.Atoi(argv[1])
+    check(err)
+
+    for i := 0; i < (tmp*4); i++ {
+        if !fileScanner.Scan() {
+            fmt.Println("Not enought values to show")
+            break;
+        }
+        fmt.Println(fileScanner.Text())
+    }
+    
+}
+//Function that reads the user input
+func readInput() string {
+    cmdString, err := reader.ReadString('\n')
+    check(err)
+
+    return cmdString
+}
+//Function that prints to the console the user Menu
+func printMenu() {
+    fmt.Println("To get the N metrics for all variables write all followed by the value of N \n(Example: all 5)\n")
+    fmt.Println("To get the N metrics for one or more variables write some\nfollowed by what variables 1-4 with commas in between and followed by the value of N \n(Example: some 1,3 5)\n")
+    fmt.Println("To get the average for one or more variables write average\nfollowed by what variables 1-4 with commas in between \n(Example: average 1,3)\n")
+    fmt.Printf("\n->")
+}
+//Function that runs in "background" generating samples each second
+func generator() {
+
+    fmt.Println("\nData is being collected\n")
+    flagGo = true
+    i := 10
+    for i>1 {                           //Infinite loop
+        var sample [samp_len] int       //Array to save vaules
+        sample = getValues()            //Getting sample values
+        writeFile(sample)               //Writing samples to file
+        time.Sleep(time.Second)         //Waiting one second
+    }
+}
+
+//Function that writes to the File that will be used to save values
+func writeFile(w [samp_len]int) {
+
+    var s string = ""
+    var tmp string
+
+    data, err := ioutil.ReadFile(fileName)
+    check(err)
+    tmp = string(data)
+
+    for _, value := range w {
+        s = fmt.Sprintf("%d\n", value)
+        tmp = s+tmp
+    }
+    err = os.Remove(fileName)
+    check(err)
+
+    f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755) // Opens file with permission and appends new values
+    check(err)
+    defer f.Close()
+
+    _, err = f.WriteString(tmp)
+    check(err)
+
+
+
+}
+
+//Function that initializes file
+func initializeFile() {
+    f, err := os.Create(fileName)
+    check(err)
+
+    defer f.Close()
+   
+}
 
 //Function that generates random integer values
 func getValues() [samp_len]int {
